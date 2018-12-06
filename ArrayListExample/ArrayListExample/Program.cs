ba837349171d91caf0148fae03e231863987c7f4
gosai09 <gosai09@gmail.com>
2018-12-06 13:41:20 +0530

    arr
    arr

---
+++ ArrayListExample/ArrayListExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //array list
+            //arr = new int[3][];
+            //arr[0] = [3,4,5,6]
+            //arr[1] = [3,4]
+            //arr[3] = [2]
+
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(4);
+            arrayList.Add(55);
+            arrayList.Add("kiran");
+            //object
+            arrayList.Insert(1, "new val");
+            arrayList.Remove(55);//by value
+            arrayList.RemoveAt(0);//by index
+
+            foreach (var element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JaggedArrayExample/JaggedArrayExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int [,] data = new int[4,4]; 16
+            int[][] jarr = new int[4][];
+
+            jarr[0] = new int[3];
+            jarr[1] = new int[2];
+            jarr[2] = new int[4];
+            jarr[3] = new int[1];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < jarr[i].Length; j++)
+                {
+                    Console.Write("\n\n\tEnter Element at {0},{1}:\t",i,j);
+                    jarr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            Console.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("\n\n");
+                for (int j = 0; j < jarr[i].Length; j++)
+                {
+                    Console.Write("\t{0}", jarr[i][j]);
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
