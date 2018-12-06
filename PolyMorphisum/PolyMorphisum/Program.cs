63c4f644881459dcb39cd5c3e53c59ea5a429bf3
gosai09 <gosai09@gmail.com>
2018-12-06 13:47:47 +0530

    OOPS 2
    OOPS2

---
+++ PolyMorphisum/PolyMorphisum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphisum
+{
+    class Program
+    {
+
+        class Calculation
+        {
+            public void process()
+            {
+                int a;
+                int b;
+                Console.Write("\n\tEnter a:\t");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("\n\tEnter b:\t");
+                b = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("\n\n\tAns:{0}", a + b);
+            }
+            public void process(int a, int b)
+            {
+
+
+                Console.WriteLine("\n\n\tAns:{0}", a + b);
+            }
+            public void process(double a, double b)
+            {
+
+
+                Console.WriteLine("\n\n\tAns:{0}", a + b);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Calculation calculation = new Calculation();
+            calculation.process();
+            calculation.process(3,4);
+            calculation.process(4.5,6.7);
+
+            Console.ReadKey();
+        }
+    }
+}
