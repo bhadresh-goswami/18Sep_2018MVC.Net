b93f66ef189c94dce451a9571a30db673ca17f1d
gosai09 <gosai09@gmail.com>
2018-12-06 13:45:27 +0530

    HTTP GET POST
    HTTP GET POST

---
+++ HTTPGETPOST/Controllers/GETPOSTEXController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTTPGETPOST.Controllers
+{
+    public class GETPOSTEXController : Controller
+    {
+        // GET: GETPOSTEX
+        [HttpGet]
+        public ActionResult Index(int? id,string name ="", string course = null)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPOST(FormCollection formCollection)
+        {
+            return View();
+        }
+    }
+}
---
+++ HTTPGETPOST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTTPGETPOST.MvcApplication" Language="C#" %>
