87eb7fe793c712cd969c682db584692be7653949
gosai09 <gosai09@gmail.com>
2018-12-06 13:39:59 +0530

    MVC1
    Package not included

---
+++ Form Helper Class/Form_HelperClasses/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_HelperClasses.Controllers
+{
+    public class UserController : Controller
+    {
+        //by default GET Method
+        //this method will execute when page load the first time
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(FormCollection formCollection)
+        {
+            string name = formCollection["txtUserName"].ToString();
+            return View();
+        }
+
+
+    }
+}
---
+++ Form Helper Class/Form_HelperClasses/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Form_HelperClasses.MvcApplication" Language="C#" %>
