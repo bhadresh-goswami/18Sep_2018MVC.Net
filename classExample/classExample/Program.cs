df58828c78d6985255002db11586fb9fc087ee41
gosai09 <gosai09@gmail.com>
2018-12-06 13:38:49 +0530

    c# 1
    C# 1

---
+++ classExample/classExample/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classExample
+{
+    //class is a collection of object
+    //class is a colleciton of data member and member  function
+    //class is the blueprint that define the form of an object
+    //class is template that define the object
+
+    class Program
+    {
+
+        class StudentClass
+        {
+
+            //data members: 90% of your data member is private to class
+            int no;///int no is private to class
+            //what do you mean by private?
+            //cant be access throught out a class
+            //only accessible inside a class
+            //variable scope is inside a class
+
+            public string name;
+            //public can be access thru-out the class
+            //not directly 
+            //need to create an instance of the class 
+            //that instance is called object or real time variable that access
+            //public function or data of class
+
+
+
+            //member functions: 90% of your functions of class is pub 
+            public void SetData()
+            {
+                Console.WriteLine("\n\n\tEnter No:\t");
+                no = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n\tEnter Name:\t");
+                name = Console.ReadLine();
+            }
+
+
+            public void Display()
+            {
+                Console.WriteLine("\n\n\tData:");
+                Console.WriteLine("\n\tNo:\t{0}\t\tName:\t{1}", no, name);
+            }
+        }
+        //class end here
+
+        static void Main(string[] args)
+        {
+
+            //create an object of class
+            StudentClass stdObj1 = new StudentClass();
+            StudentClass stdObj2 = new StudentClass();
+            StudentClass stdObj3 = new StudentClass();
+            StudentClass stdObj4 = new StudentClass();
+
+            stdObj1.SetData();
+            stdObj2.SetData();
+            stdObj3.SetData();
+            stdObj4.SetData();
+            Console.ReadKey();
+            Console.Clear();
+
+            stdObj1.Display();
+            stdObj2.Display();
+            stdObj3.Display();
+            stdObj4.Display();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ thisKeyWord/thisKeyWord/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thisKeyWord
+{
+    class Program
+    {
+
+        //precap 
+        //what is class?
+        //what is an object?
+        //what is data member and member function of class?
+        //use of public and private
+        //acceess the public member of class using object
+
+        #region This Key Word
+
+        class MyClassDemo
+        {
+            int a;//scope inside a class
+            int b;
+
+            //here a is argument or param of function
+            public void setData(int a,int b)//a has scope inside only function
+            {
+                //we cant use this directly => a = a;
+                this.a = a;
+                this.b = b;
+
+                //this.a is a member of class
+                //means this is an active object of class
+            }
+        }
+        #endregion
+
+        #region Static Key Word
+        class MyVariables
+        {
+            //simple public or private member of class can store the memory
+            //for an object
+            //means as per object the member is diff
+            public int m_a;
+
+            //static pub or private can only access by class itself
+            //static member allocate memory for once for class
+            public static int s_a;
+
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            MyVariables varObj1 = new MyVariables();
+            MyVariables varObj2 = new MyVariables();
+            MyVariables varObj3 = new MyVariables();
+            MyVariables varObj4 = new MyVariables();
+
+
+            Console.WriteLine("\n\n");
+            varObj1.m_a++;//?? why s_a is not appear in object
+            varObj2.m_a++;
+            varObj3.m_a++;
+            varObj4.m_a++;
+
+
+            Console.WriteLine("\t\tVarObj 1 m_a:{0}", varObj1.m_a);
+            Console.WriteLine("\t\tVarObj 2 m_a:{0}", varObj2.m_a);
+            Console.WriteLine("\t\tVarObj 3 m_a:{0}", varObj3.m_a);
+            Console.WriteLine("\t\tVarObj 4 m_a:{0}", varObj4.m_a);
+
+
+
+            Console.WriteLine("\n\n");
+            MyVariables.s_a++;//?? why s_a is appear in class because it is static and only create for class
+            MyVariables.s_a++;
+            MyVariables.s_a++;
+            MyVariables.s_a++;
+
+
+            Console.WriteLine("\t\tMyVariables s_a:{0}", MyVariables.s_a++);
+            Console.ReadKey();
+
+
+        }
+    }
+}
