40570a7156295d64c8ee17f3cc8de0b14846424e
gosai09 <gosai09@gmail.com>
2018-12-06 13:44:28 +0530

    Session Variables
    ViewBag, ViewData, TempData

---
+++ viewbag-tempdata-viewdata/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ViwBagViewDataTempData.Models;
+
+
+namespace ViwBagViewDataTempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region ViewBag Login Example
+
+        [HttpGet]
+        public ActionResult InsertData()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertData(InfoModel infoModel)
+        {
+            if (infoModel.Password == "MTIzNA==" && infoModel.UserName == "abc")
+            {
+                ViewBag.message = "User Name Or Password is True!";
+            }
+            else
+            {
+                ViewBag.error = "User Name Or Password is wrong!";
+            }
+            return View();
+        }
+
+        #endregion
+
+        #region ViewBag
+        public ActionResult ViewBagExample()//act1
+        {
+            ViewBag.name = "Bhadresh";
+            ViewBag.bs = 12000.00;
+            ViewBag.inc = 12.20;
+
+            TempData["ticks"] = DateTime.Now.Ticks.ToString();
+
+            return View();
+        }
+        #endregion
+        #region ViewData
+        public ActionResult ViewDataExample()//act2
+        {
+            ViewData["name"] = "Bhadresh";
+            ViewData["bs"] = 12000.00;
+            ViewData["inc"] = 12.20;
+
+
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ viewbag-tempdata-viewdata/Models/InfoModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViwBagViewDataTempData.Models
+{
+    public class InfoModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        //private string _UserName;
+
+        //public string UserName
+        //{
+        //    get { return _UserName; }
+        //    set { _UserName = value; }
+        //}
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+
+        private string _Password;
+        [DataType(DataType.Password)]
+        public string Password
+        {
+            get
+            {
+                //System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                //System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                //byte[] todecode_byte = Convert.FromBase64String(_Password);
+                //int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+                //char[] decoded_char = new char[charCount];
+                //utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+                //string result = new String(decoded_char);
+
+
+                //return result;
+                return _Password;
+            }
+            set
+            {
+
+                byte[] encData_byte = new byte[value.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(value);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                _Password = encodedData;
+
+            }
+        }
+
+
+
+    }
+}
